f98ebf4f6de908425d5b65b3ad52d222381fcd5d
beyondkoma <850239158@qq.com>
2014-11-16 16:16:22 +0800

    update direc

--- schemestudy/sicp1section/test.rkt
+++
@@ -1,8 +0,0 @@
-(define (factorial n)
-  (facter 1 1 n))
-
-(define (facter product counter max-count)
-    (if (> counter max-count)
-        product
-        (facter (* counter product) (+ counter 1)
-                max-count)))
