99307f355f139370057791cfcafd0b4bfa968f57
beyondkoma <850239158@qq.com>
2015-01-25 21:44:26 +0800

    add new 3rd chapter content

--- schemestudy/sicp3section/3.2.3.scm
+++ schemestudy/sicp3section/3.2.3.scm
@@ -8,8 +8,8 @@
 	    "Insufficient funds")))
 
 (define W1 (make-withdraw 100))
-
 (W1 50)
 
 ;;; 3.10
-
+一样的，唯一的区别是，多了一个局部环境变量 balance，原来的结构仅使用形式参数
+initial-amount 来维持局部变量状态即可。
---
+++ schemestudy/sicp3section/3.3.1.scm
@@ -0,0 +1,59 @@
+
+
+;;; 变动的表结构
+
+(define (cons x y)
+   (let ((new (get-new-pair)))
+          (set-car! new x)
+	  (set-cdr! new y)))
+
+;;; 共享和相等
+(define x (list 'a 'b))
+(define z1 (cons x x))
+(define (set-to-wow! x)
+  (set-car!  (car x)  'wow)
+ x)
+
+z1 
+(set-to-wow! z1)
+
+
+z2
+(set-to-wow! z2)
+
+
+
+;;; 改变也就是赋值
+
+;; (define (cons x y)
+;;    (define (dispatch m)
+;;          (cond  ((eq? m 'car) x)
+;; 		     ((eq? m 'cdr) y)
+;; 		     (else (error "Undefined operation -- CONS" m))))
+;;    dispatch)
+
+;; (define (car z) (z 'car))
+;; (define (cdr z) (z 'cdr))
+
+(define (cons x y)
+     (define (set-x! v) (set! x v))  
+     (define (set-y! v) (set! y v))
+     (define (dispatch m)
+           (cond ((eq? m 'car) x)
+		      ((eq? m 'cdr) y)
+		      ((eq? m 'set-car!) set-x!)
+		      ((eq? m 'set-cdr!) set-y!)
+		      (else (error "Undefined operation -- CONS"  m))))
+  dispatch)
+
+(define (car z) (z 'car))
+(define (cdr z) (z 'cdr))
+(define (set-car! z new-value)
+  ((z' set-car!) new-value)
+  z)
+
+(define (set-cdr! z new-value)
+  ((z' set-cdr!) new-value)
+z)
+
+
