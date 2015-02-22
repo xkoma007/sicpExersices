b42bff8abcd2286ffee2f7d675049107a757f141
beyondkoma <850239158@qq.com>
2015-02-22 23:04:26 +0800

    3.3.3.scm

--- schemestudy/sicp3section/3.3.3.scm
+++ schemestudy/sicp3section/3.3.3.scm
@@ -1,6 +1,17 @@
 
 
 ;;; 表格的表示
+        (define  (lookup key table)
+	        (let ((record (assoc key (cdr table))))
+		   (if record
+		       (cdr record)
+		       false)))
+
+      (define (assoc key records)
+	    (cond ((null? records) false)
+		      ((equal? key (car records))  (car records))
+		      (else (assoc key (cdr records)))))
+     
 
 ;;; 两维表格
 
